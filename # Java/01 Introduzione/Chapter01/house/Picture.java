cae2b971ee3671ae04de3aba678c4da98a9b328b
Talpa <luppialessandro@istitutocorni.it>
2025-09-26 12:55:59 +0200

    Esercizio

--- # Java/01 Introduzione/Chapter01/house/Picture.java
+++ # Java/01 Introduzione/Chapter01/house/Picture.java
@@ -6,7 +6,7 @@
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
+ * @author  Michael Kï¿½lling and David J. Barnes
  * @version 7.0
  */
 public class Picture
@@ -15,7 +15,6 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
-    private Circle sun2;
     private boolean drawn;
 
     /**
@@ -27,7 +26,6 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
-        sun2 = new Circle();
         drawn = false;
     }
 
@@ -58,13 +56,6 @@ public class Picture
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
-            drawn = true;
-            
-            sun2.changeColor("red");
-            sun2.moveHorizontal(150);
-            sun2.moveVertical(-40);
-            sun2.changeSize(80);
-            sun2.makeVisible();
             drawn = true;
         }
     }
