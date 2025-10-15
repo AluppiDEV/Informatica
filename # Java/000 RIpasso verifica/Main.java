851e4320d87c50353d560828eb97f127705cf6c4
Talpa <luppialessandro@istitutocorni.it>
2025-10-15 09:58:18 +0200

    Update

---
+++ # Java/000 RIpasso verifica/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String pippo = new String("CIAO");
+        String pera = new String("CIAO");
+
+        if (pippo == pera) {
+            System.out.println("VERO");
+        } else {
+            System.out.println("FALSO");
+        }
+    }
+}
