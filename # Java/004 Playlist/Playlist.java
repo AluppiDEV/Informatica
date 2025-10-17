73855eaca358139f164c32221098c1bd380b2854
Alessandro Luppi <luppialessandro@istitutocorni.it>
2025-10-17 10:19:23 +0200

    Update

--- # Java/004 Playlist/Playlist.java
+++ # Java/004 Playlist/Playlist.java
@@ -2,6 +2,36 @@ import java.util.ArrayList;
 
 public class Playlist {
     
-    private ArrayList<Canzone>;
+    private ArrayList<Canzone> playlist;
+
+    public Playlist() {
+
+    }
+
+    public void aggiungiCanzone(Canzone c) {
+        playlist.add(c);
+    }
+
+    public void rimuoviCanzone(String titolo) {
+        int index = 0;
+        for (Canzone c : playlist) {
+            if (c.getTitolo().equals(titolo)) {
+                index = playlist.indexOf(c);
+            }
+        }
+        playlist.remove(index);
+    }
+
+    public String visualizzaPlaylist() {
+        String out = "";
+        int totalTime = 0;
+
+        for (Canzone c : playlist) {
+            out += c.toString() + "\n";
+            totalTime += c.getDurata();
+        }
+
+        return out + totalTime;
+    }
 
 }
